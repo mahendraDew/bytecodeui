e0f0ce99182c542ca1ecda14025a73a976f15d01
mahendra <mahendradewangan195@gmail.com>
2024-12-17 16:55:40 +0530

    org cd

--- components/card-container.tsx
+++ components/card-container.tsx
@@ -1,30 +1,27 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link"
 
-type CardContainerProps = {
+type CardComponentProps = {
   children: React.ReactNode
+  slug: string
   name: string
   description: string
-  slug: string
 }
 
-export default function CardContainer ({
+export const CardContainer = ({
   children,
+  slug,
   name,
-  description,
-  slug
-}: CardContainerProps) {
-  return (
-    <Link href={"/ui/" + slug}>
-      <div className='relative flex h-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-[radial-gradient(#ffffff12_1px,transparent_1px)] bg-[size:16px_16px] py-10'>
-        {children}
-      </div>
-      <p className='mb-2 mt-4 text-xl font-bold text-neutral-700 dark:text-neutral-100'>
-        {name}
-      </p>
-      <p className='text-muted-foreground mt-2 text-sm font-normal'>
-        {description}
-      </p>
-    </Link>
-  )
-}
+  description
+}: CardComponentProps) => (
+  <Link href={`/${slug}`}>
+    <div className="relative flex h-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-[radial-gradient(#ffffff12_1px,transparent_1px)] bg-[size:16px_16px] py-10">
+      {children}
+    </div>
+    <p className="mb-2 mt-4 text-xl font-bold text-neutral-700 dark:text-neutral-100">
+      {name}
+    </p>
+    <p className="text-muted-foreground mt-2 text-sm font-normal">
+      {description}
+    </p>
+  </Link>
+)
